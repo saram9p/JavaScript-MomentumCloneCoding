2674f409640b1b791562744301ab555a902ce4da
saram <saram9p@gmail.com>
2022-01-17 22:09:12 +0900

    1. 로그인 기능(localStorage 활용)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
-const loginInput = document.querySelector("#login-form input"); // id가 login-form 인 element에서 input을 찾는다
-const loginButton = document.querySelector("#login-form button"); // id가 login-form 인 element에서 button 찾는다
+const loginForm = document.querySelector("#login-form"); 
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick(){
-    console.dir(loginInput);
-    console.log("클릭!");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"; // string만 포함된 변수는 대문자로 표기
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event){ // 이 함수가 하나의 argument를 받도록 하고 있다.
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username); // 앞은 저장될 아이템의 이름이고 뒤는 값이될 변수다.
+    paintGreetings(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+function paintGreetings(username){ // 함수를 호출하는 위치에 따라 유저정보는 다른 곳에서 온다.
+    greeting.innerText = `Hello ${username}`; // greeting에 텍스트를 추가 한다
+    greeting.classList.remove(HIDDEN_CLASSNAME); // greeting한테서 HIDDEN_CLASSNAME을 제거한다
+}
+
+const  savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME); // loginForm한테서 HIDDEN_CLASSNAME을 제거한다
+    loginForm.addEventListener("submit", onLoginSubmit); // loginForm의 submit을 작동시킨다.
+} else {
+    paintGreetings(savedUsername);
+}
